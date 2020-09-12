18318b78db63f70b91bfd3d485bbf099d23f6e31
Sarah Valme <svalm97@gmail.com>
2020-09-11 21:28:34 -0400

    new server code

--- index.js
+++ index.js
@@ -1,29 +1,52 @@
 const http = require('http');
 const fs = require('fs');
+var path = require('path');
+const url = require('url');
 
-const server = http.createServer((req, res) => {
+const server = http.createServer((request, res) => {
   
-  var pathOfPage;
+  var u = url.parse(request.url, true);
+  var filePath = '.' + u.pathname;
+
+  if (filePath === './') 
+    filePath = './views/pages/index.html';
+
+  var extname = path.extname(filePath);
+  var contentType = 'text/html';
+  console.log("extension name: " + extname);
+  switch (extname) {
+    case '.html':
+      contentType = 'text/html';
+      break;
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.ico':
+      contentType = 'image/x-icon';
+      break;
+  }
 
   console.log("console check.");
+  console.log("contentType: " + contentType);
 
-  if (req.url === '/') {
+  if(filePath != './favicon.ico') {
 
-    pathOfPage = './views/pages/index.html';
-
-    fs.readFile(pathOfPage, (err, data) => {
+    fs.readFile(filePath, (err, data) => {
       if (err) throw err;
-      res.writeHead(200, 'Content-Type', 'text/html');
+      res.writeHead(200, { 'Content-Type': contentType });
       res.write(data);
       res.end();
     });
-  } else {
-    res.writeHead(200, {'Content-Type': 'image/x-icon'});
-    res.end();
-  }
 
+  } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end();
+  };
   
 });
 
-// start the server on port 5000
+// start the server on port 
 server.listen(process.env.PORT);
