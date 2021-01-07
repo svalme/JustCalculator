9f8141536954ecef5c2972c4c8fda710325c7199
Sarah Valme <svalm97@gmail.com>
2021-01-07 18:52:38 -0500

    added mathquill

--- javascript/process.js
+++ javascript/process.js
@@ -10,3 +10,16 @@ function displayNewSteps() {
     testFunction();
     let testClass = new TestClass();
 }
+
+var mathFieldSpan = document.getElementById('math-field');
+var latexSpan = document.getElementById('latex');
+
+var MQ = MathQuill.getInterface(2); // for backcompat
+var mathField = MQ.MathField(mathFieldSpan, {
+  //spaceBehavesLikeTab: true, // configurable
+  handlers: {
+    edit: function() { // useful event handlers
+      latexSpan.textContent = mathField.latex(); // simple API
+    }
+  }
+});
